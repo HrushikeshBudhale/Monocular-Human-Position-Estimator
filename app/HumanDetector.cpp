3ee2790442a3c416c796ca94adb5a3238af35cca
Abhijit Mahalle <abhijeetmahalle27@gmail.com>
2021-10-14 11:39:43 -0400

    Updated Human Detector Stub

--- app/HumanDetector.cpp
+++ app/HumanDetector.cpp
@@ -1,9 +1,28 @@
+/**
+ * @file HumanDetector.cpp
+ * @author Driver: Abhijit Mahalle, Navigator: Hrushikesh Budhale
+ * @brief Library for HumanDetector class
+ * @version 0.1
+ * @date 2021-10-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include "../include/HumanDetector.hpp"
 #include <iostream>
+
+/**
+ * @brief Default Constructor for Human Detector class
+ */
 HumanDetector::HumanDetector() {
     detected_people = 0;
+    // todo: add contructor logic
 }
 
+/**
+ * @brief Function to print positions of humans i
+ *        n robot frame
+ */
 void HumanDetector::get_positions() {
-    std::cout << "Output";
+    // todo: add get position logic
 }
--- include/HumanDetector.hpp
+++ include/HumanDetector.hpp
@@ -1,8 +1,22 @@
+/**
+ * @file HumanDetector.hpp
+ * @author Driver: Abhijit Mahalle, Navigator: Hrushikesh Budhale
+ * @brief Library for HumanDetector class
+ * @version 0.1
+ * @date 2021-10-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #pragma once
 
 #include <map>
 #include <vector>
 
+/**
+ * @brief High level class for detecting humans in robot frame
+ * 
+ */
 class HumanDetector {
  public:
     HumanDetector();
