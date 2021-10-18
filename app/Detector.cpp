cccb1b8952877422691fd0e89c1e5a1b1845ced2
Abhijit Mahalle <abhijeetmahalle27@gmail.com>
2021-10-17 23:48:14 -0400

    Implemented Detector class

--- app/Detector.cpp
+++ app/Detector.cpp
@@ -10,13 +10,17 @@
  */
 
 #include "../include/Detector.hpp"
+#include "iostream"
 
 /**
  * @brief Default constructor for Detector
  * 
  */
 Detector::Detector() {
-    
+    camera = cv::VideoCapture();
+    camera.open(0);
+    fps = camera.get(cv::CAP_PROP_FPS);
+    hog_detector.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
 }
 
 /**
@@ -24,7 +28,10 @@ Detector::Detector() {
  * @param camera_id int : camera id
  */
 Detector::Detector(int camera_id) {
-    // todo: initialize camera and HOG detector
+    cv::VideoCapture camera = cv::VideoCapture();
+    camera.open(camera_id);
+    fps = camera.get(cv::CAP_PROP_FPS);
+    hog_detector.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
 }
 
 /**
@@ -33,7 +40,28 @@ Detector::Detector(int camera_id) {
  * @param file_path 
  */
 Detector::Detector(std::string file_path) {
-    // todo: initialize HOG detector and read file file_path
+    cv::VideoCapture camera = cv::VideoCapture();
+    camera.open(file_path);
+    fps = camera.get(cv::CAP_PROP_FPS);
+    hog_detector.setSVMDetector(cv::HOGDescriptor::getDefaultPeopleDetector());
+}
+
+/**
+ * @brief Updates private member like detections and frame by performing
+ *        hog detection.
+ * 
+ */
+void Detector::detect_humans() {
+    if (!camera.isOpened()) {
+        std::cout << "Cannot open the source. \n";
+        return;
+    }
+    if (!camera.read(frame)) {
+        std::cout << "\n Cannot read the frame. \n";
+        return;
+    }
+    cv::resize(frame, frame, cv::Size(), 0.3, 0.3);
+    hog_detector.detectMultiScale(frame, detections);
 }
 
 /**
@@ -42,9 +70,7 @@ Detector::Detector(std::string file_path) {
  * @return std::vector<cv::Rect> 
  */
 std::vector<cv::Rect> Detector::get_detections() {
-    std::vector<cv::Rect> v;
-
-    return v;
+    return detections;
 }
 
 /**
@@ -53,8 +79,12 @@ std::vector<cv::Rect> Detector::get_detections() {
  * @return std::vector<cv::Point2d> 
  */
 std::vector<cv::Point2d> Detector::get_centroid() {
-    std::vector<cv::Point2d> v;
-    return v;
+    std::vector<cv::Point2d> points;
+    for (auto& detection : detections) {
+        points.push_back(cv::Point2d(detection.x + (detection.width/2),
+                               detection.y + (detection.height/2)));
+    }
+    return points;
 }
 
 /**
@@ -62,7 +92,11 @@ std::vector<cv::Point2d> Detector::get_centroid() {
  * 
  */
 void Detector::show_output() {
-    // todo: show detector output with bounding boxes around detected person
+    cv::imshow("Frame1", frame);
+    int key = cv::waitKey(static_cast<int>(1000.0/fps));
+    if (key == 27 || static_cast<char>(key) == 'q') {
+        cv::destroyAllWindows();
+    }
 }
 
 /**
@@ -71,5 +105,12 @@ void Detector::show_output() {
  * @param boxPtr Pointer to cv::Rect object created after detections
  */
 void Detector::resize_bounding_box(cv::Rect* boxPtr) {
-    // todo: resize the bounding box to fixed values.
+    boxPtr->x += static_cast<int>(boxPtr->width*0.5);
+    boxPtr->width = static_cast<int>(boxPtr->width*0.8);
+    boxPtr->y += static_cast<int>(boxPtr->height*0.3);
+    boxPtr->height = static_cast<int>(boxPtr->height*0.8);
+}
+
+Detector::~Detector() {
+    camera.release();
 }
