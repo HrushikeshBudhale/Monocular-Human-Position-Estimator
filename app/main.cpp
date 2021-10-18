22c1dbe27e9c6472db2efbf48dab92a14122349c
Abhijit Mahalle <abhijeetmahalle27@gmail.com>
2021-10-17 23:55:58 -0400

    Included dependencies

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,20 @@
+/**
+ * @file main.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #include <iostream>
-#include <.hpp>
+#include "../include/PoseTransformer.hpp"
+#include "../include/HumanDetector.hpp"
+#include "../include/Detector.hpp"
 
 int main() {
-    dummy();
+    // auto transformer1 = PoseTransformer();
     return 0;
 }
