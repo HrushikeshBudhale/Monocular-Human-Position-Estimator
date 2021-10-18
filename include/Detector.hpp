1d3baacf3e29cc11f299160ad6d2b96448e4972a
Abhijit Mahalle <abhijeetmahalle27@gmail.com>
2021-10-18 13:58:57 -0400

    Updated dependency for Detector.hpp

--- include/Detector.hpp
+++ include/Detector.hpp
@@ -14,10 +14,10 @@
 
 #include <vector>
 #include <string>
-#include <opencv2/highgui.hpp>
-#include <opencv2/objdetect.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/videoio.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/videoio/videoio.hpp>
 
 
 class Detector {
