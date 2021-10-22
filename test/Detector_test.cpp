2795efb61507d25388ff8fafcb25f8c78a8d87bd
HrushikeshBudhale <hruhnb@gmail.com>
2021-10-21 23:17:52 -0400

    added test file for Detector class

---
+++ test/Detector_test.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file Detector_test.cpp
+ * @author Driver: Hrushikesh Budhale, Navigator: Abhijit Mahalle
+ * @brief 
+ * @version 0.1
+ * @date 2021-10-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <gtest/gtest.h>
+#include "../include/Detector.hpp"
+
+TEST(Test_Detector, test_explicit_constructor) {
+    EXPECT_EQ(1, 1);
+}
