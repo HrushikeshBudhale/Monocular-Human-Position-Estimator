b1da72817d9e90faf111c44bc39606eb9b58308b
HrushikeshBudhale <hruhnb@gmail.com>
2021-10-24 21:00:00 -0400

    Removed show output function call  from test

--- test/HumanDetector_test.cpp
+++ test/HumanDetector_test.cpp
@@ -45,7 +45,6 @@ TEST(Test_HumanDetector, test_default_constructor) {
 TEST(Test_HumanDetector, track_position_test) {
     auto human_detector = HumanDetector("../1_person.jpg");
     auto points_3d = human_detector.track_positions();
-    human_detector.show_output();
     std::vector<cv::Point3d> expected_points;
     expected_points.push_back(cv::Point3d(-1.0, 0.5, 3.0));
     ASSERT_EQ(1, static_cast<int>(points_3d.size()));
